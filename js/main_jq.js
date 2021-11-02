6b435fd2245be194f11f7995fa93616a81433c57
BOHDANff <45514703+BOHDANff@users.noreply.github.com>
2021-11-02 20:34:26 +0200

    Add files via upload

--- js/main_jq.js
+++ js/main_jq.js
@@ -1,16 +1,24 @@
 $(document).ready(function(){
   $('.slider').slick({
-
   	infinite: true,
   	speed: 800,
-  	// dots: true,
+  	// dots: false,
   	autoplay: true,
   	autoplaySpeed: 5000,
   	// centerMode: true,
   	// centerPadding: '80px',
   	prevArrow: '<img class="slider__arrow slider__arrow-prev" src="./images/arrov_prev.svg" alt="slider prevArrow">',
   	nextArrow: '<img class="slider__arrow slider__arrow-next" src="./images/arrov_next.svg" alt="slider nextArrow">',
-  });
+    responsive: [
+    {
+      breakpoint: 640,
+      settings: {
+        arrows: false,
+        dots: true
+      }
+    }
+  ]
+});
   $('.header__burger').click(function(event) {
     $('.header__burger,.menu').toggleClass('active');
     $('body').toggleClass('lock');
